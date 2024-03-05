25731615f10d84d7e9023ab8d10d21722fad8c86
ajaypatel0 <krishnapc700@gmail.com>
2024-03-05 08:42:27 +0530

    [ADDED]: Ajay - java program added with a welcome message

---
+++ src/lineComparisonComputation/LineComparisionComputation.java
@@ -0,0 +1,9 @@
+package lineComparisonComputation;
+
+public class LineComparisionComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision computation program");
+	}
+
+}
