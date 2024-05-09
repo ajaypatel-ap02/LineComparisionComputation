32c81cbcc4b95525ca002e91a7c6bafc532f4eba
ajaypatel-ap02 <26patilajay@gmail.com>
2024-05-09 14:14:54 +0530

    [MODIFIED]:Ajay - UC completed for check the equality of two lines

--- src/com/Ajay/lineComparison/LineComparison.java
+++ src/com/Ajay/lineComparison/LineComparison.java
@@ -4,25 +4,42 @@ import java.util.Scanner;
 
 public class LineComparison {
 
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	
+		
+	public LineComparison(int x1, int y1, int x2, int y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	public double lengthOfLine() {
+		
+		double length = Math.sqrt(Math.pow(this.x2-this.x1, 2) + Math.pow(this.y2-this.y1, 2));
+		System.out.println("Length of a line is "+length);
+		
+		return length;
+	}
+	public void checkEquality(LineComparison lineComparison) {
+		if (this.lengthOfLine() == lineComparison.lengthOfLine()) {
+			System.out.println("Both lines are same");
+		}else {
+			System.out.println("Lines are not equal");
+		}
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Line Comparison Program ");
 		
-		Scanner scanner = new Scanner(System.in);
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		double lengthOfLine;
+		LineComparison lineComparison1 = new LineComparison(20, 10, 40, 30);
+		LineComparison lineComparison2 = new LineComparison(40, 30, 20, 10);
 		
-		System.out.println("Enter two points X1 & Y1 , X2 & Y2");
-		x1 = scanner.nextInt();
-		y1 = scanner.nextInt();
-		x2 = scanner.nextInt();
-		y2 = scanner.nextInt();
-		
-		lengthOfLine = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-		System.out.println("Length of a line is "+lengthOfLine);
+		lineComparison1.checkEquality(lineComparison2);
 	}
 
 }
